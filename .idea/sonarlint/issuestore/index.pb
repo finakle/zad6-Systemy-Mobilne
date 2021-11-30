f04fbb8b6ea138e9cd13d46b5682a639f9d431f1
finakle <justynaputko@interia.pl>
2021-11-30 11:30:38 +0100

    zadanie 6 SM

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+e
+5app/src/main/java/pl/edu/pb/zadanie6/TaskStorage.java,1\e\1edc86935ed1c83f76a2a752580bd9f2f2529f09
+j
+:app/src/main/java/pl/edu/pb/zadanie6/TaskListFragment.java,1\6\16cd189a87f2e3f70e3f99338cbd28eb6b59f14d
---
+++ app/src/main/java/pl/edu/pb/zadanie6/TaskListActivity.java
@@ -0,0 +1,13 @@
+package pl.edu.pb.zadanie6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class TaskListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new TaskListFragment();
+    }
+}
